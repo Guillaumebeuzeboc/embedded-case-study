ae80bb644ba619ddaefcc25c94033f0cfd7efe08
Nicolas Pichon <pichon.nicolas1@gmail.com>
2021-07-10 13:35:27 +0200

    Merge branch 'develop' of https://github.com/eatcala/embedded-case-study into develop

--- controllers/commander.py
+++ controllers/commander.py
@@ -1,4 +1,5 @@
 import asyncio
+from typing import List, Any
 
 from .fake_can_library import CAN
 
@@ -8,21 +9,23 @@ ARBITRARY_DURATION = 30
 class Commander:
     def __init__(self) -> None:
         self.loop = asyncio.get_event_loop()
-        self.pcb_messages = []
-        self.can = CAN(self.pcb_messages)
+        self.reception_pcb_messages_queue = []
+        self.can = CAN(self.reception_pcb_messages_queue)
         self.loop.create_task(self.service())
-        self.loop.create_task(self.fake_can_reception())
+        self.loop.create_task(self.fake_pcb_message_reception())
 
     async def service(self) -> None:
         print("Main loop")
+        # The while below is an example to show you how you receive
+        # pcb message when the system has done the task you asked him to do
         while(True):
             await asyncio.sleep(1)
-            print(f"{self.pcb_messages}")
+            print(f"{self.reception_pcb_messages_queue}")
 
-    async def fake_can_reception(self) -> None:
+    async def fake_pcb_message_reception(self) -> None:
         while(True):
-            self.pcb_messages.append({
+            self.reception_pcb_messages_queue.append({
                 "system_address": 0x80,
                 "can_message": ['0x08', '0x0', '0x0', '0x01']
-            })  # STRAINER_IN_FLIPPER
+            })  # STRAINER_TO_FLIPPER
             await asyncio.sleep(ARBITRARY_DURATION)
